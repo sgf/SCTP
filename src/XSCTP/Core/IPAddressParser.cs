d49b02549fd3b86d248755e5e063189aeb749e2f
jamie.shi <jamie.shi@penseesoft.com>
2019-05-20 13:41:15 +0800

    隐藏简历按钮

--- src/XSCTP/Core/IPAddressParser.cs
+++ src/XSCTP/Core/IPAddressParser.cs
@@ -8,7 +8,233 @@ using System.Text;
 namespace XSCTP
 {
 
-    internal partial struct IPv4
+
+    internal static class IPv4AddressHelper
+    {
+        // Token: 0x0600000B RID: 11 RVA: 0x00005734 File Offset: 0x00004134
+        internal unsafe static int ParseHostNumber(ReadOnlySpan<char> str, int start, int end)
+        {
+            byte* ptr = stackalloc byte[4];
+            IPv4AddressHelper.ParseCanonical(str, ptr, start, end);
+            return ((*ptr) << 24) + (ptr[1] << 16) + (ptr[2] << 8) + ptr[3];
+        }
+
+        // Token: 0x0600000C RID: 12 RVA: 0x00005769 File Offset: 0x00004169
+        internal unsafe static bool IsValid(char* name, int start, ref int end, bool allowIPv6, bool notImplicitFile, bool unknownScheme)
+        {
+            if (allowIPv6 || unknownScheme)
+            {
+                return IPv4AddressHelper.IsValidCanonical(name, start, ref end, allowIPv6, notImplicitFile);
+            }
+            return IPv4AddressHelper.ParseNonCanonical(name, start, ref end, notImplicitFile) != -1L;
+        }
+
+        // Token: 0x0600000D RID: 13 RVA: 0x00005790 File Offset: 0x00004190
+        private unsafe static bool ParseCanonical(ReadOnlySpan<char> name, byte* numbers, int start, int end)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                byte b = 0;
+                char c;
+                while (start < end && (c = (name[start])) != '.' && c != ':')
+                {
+                    b = (byte)(b * 10 + (c - '0'));
+                    start++;
+
+                }
+                numbers[i] = b;
+                start++;
+            }
+            return *numbers == 127;
+        }
+
+        // Token: 0x0600000E RID: 14 RVA: 0x000057E8 File Offset: 0x000041E8
+        internal unsafe static bool IsValidCanonical(char* name, int start, ref int end, bool allowIPv6, bool notImplicitFile)
+        {
+            int num = 0;
+            int num2 = 0;
+            bool flag = false;
+            bool flag2 = false;
+            while (start < end)
+            {
+                char c = name[start];
+                if (allowIPv6)
+                {
+                    if (c == ']' || c == '/')
+                    {
+                        break;
+                    }
+                    if (c == '%')
+                    {
+                        break;
+                    }
+                }
+                else if (c == '/' || c == '\\' || (notImplicitFile && (c == ':' || c == '?' || c == '#')))
+                {
+                    break;
+                }
+                if (c <= '9' && c >= '0')
+                {
+                    if (!flag && c == '0')
+                    {
+                        if (start + 1 < end && name[start + 1] == '0')
+                        {
+                            return false;
+                        }
+                        flag2 = true;
+                    }
+                    flag = true;
+                    num2 = num2 * 10 + (int)(name[start] - '0');
+                    if (num2 > 255)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (c != '.')
+                    {
+                        return false;
+                    }
+                    if (!flag || (num2 > 0 && flag2))
+                    {
+                        return false;
+                    }
+                    num++;
+                    flag = false;
+                    num2 = 0;
+                    flag2 = false;
+                }
+                start++;
+            }
+            bool flag3 = num == 3 && flag;
+            if (flag3)
+            {
+                end = start;
+            }
+            return flag3;
+        }
+
+        // Token: 0x0600000F RID: 15 RVA: 0x000058E0 File Offset: 0x000042E0
+        internal unsafe static long ParseNonCanonical(char* name, int start, ref int end, bool notImplicitFile)
+        {
+            long* ptr = stackalloc long[(UIntPtr)32];
+            long num = 0L;
+            bool flag = false;
+            int num2 = 0;
+            int i;
+            for (i = start; i < end; i++)
+            {
+                char c = name[i];
+                num = 0L;
+                int num3 = 10;
+                if (c == '0')
+                {
+                    num3 = 8;
+                    i++;
+                    flag = true;
+                    if (i < end)
+                    {
+                        c = name[i];
+                        if (c == 'x' || c == 'X')
+                        {
+                            num3 = 16;
+                            i++;
+                            flag = false;
+                        }
+                    }
+                }
+                while (i < end)
+                {
+                    c = name[i];
+                    int num4;
+                    if ((num3 == 10 || num3 == 16) && '0' <= c && c <= '9')
+                    {
+                        num4 = (int)(c - '0');
+                    }
+                    else if (num3 == 8 && '0' <= c && c <= '7')
+                    {
+                        num4 = (int)(c - '0');
+                    }
+                    else if (num3 == 16 && 'a' <= c && c <= 'f')
+                    {
+                        num4 = (int)(c + '\n' - 'a');
+                    }
+                    else
+                    {
+                        if (num3 != 16 || 'A' > c || c > 'F')
+                        {
+                            break;
+                        }
+                        num4 = (int)(c + '\n' - 'A');
+                    }
+                    num = num * (long)num3 + (long)num4;
+                    if (num > (long)((ulong)-1))
+                    {
+                        return -1L;
+                    }
+                    flag = true;
+                    i++;
+                }
+                if (i >= end || name[i] != '.')
+                {
+                    break;
+                }
+                if (num2 >= 3 || !flag || num > 255L)
+                {
+                    return -1L;
+                }
+                ptr[num2] = num;
+                num2++;
+                flag = false;
+            }
+            if (!flag)
+            {
+                return -1L;
+            }
+            if (i < end)
+            {
+                char c;
+                if ((c = name[i]) != '/' && c != '\\' && (!notImplicitFile || (c != ':' && c != '?' && c != '#')))
+                {
+                    return -1L;
+                }
+                end = i;
+            }
+            ptr[num2] = num;
+            switch (num2)
+            {
+                case 0:
+                    if (*ptr > (long)((ulong)-1))
+                    {
+                        return -1L;
+                    }
+                    return *ptr;
+                case 1:
+                    if (ptr[1] > 16777215L)
+                    {
+                        return -1L;
+                    }
+                    return *ptr << 24 | (ptr[1] & 16777215L);
+                case 2:
+                    if (ptr[2] > 65535L)
+                    {
+                        return -1L;
+                    }
+                    return *ptr << 24 | (ptr[1] & 255L) << 16 | (ptr[2] & 65535L);
+                case 3:
+                    if (ptr[3] > 255L)
+                    {
+                        return -1L;
+                    }
+                    return *ptr << 24 | (ptr[1] & 255L) << 16 | (ptr[2] & 255L) << 8 | (ptr[3] & 255L);
+                default:
+                    return -1L;
+            }
+        }
+    }
+
+internal partial struct IPv4
     {
         internal unsafe static IPv4? Parse(ReadOnlySpan<char> ipSpan)
         {
@@ -225,7 +451,7 @@ namespace XSCTP
         {
             int length = ipSpan.Length;
             bool flag;
-            fixed (char* reference = MemoryMarshal.GetReference(ipSpan))
+            fixed (char* reference = &MemoryMarshal.GetReference(ipSpan))
             {
                 char* name = reference;
                 flag = IPv6AddressHelper.IsValidStrict(name, 0, ref length);
