2eb3fe4653b709d9a368d2f5a904a96510b1b792
Soar <supersoar@outlook.com>
2019-05-22 11:00:15 +0800

    t提交

--- src/XSCTP/Core/Class1.cs
+++ src/XSCTP/Core/Class1.cs
@@ -6,21 +6,62 @@ using System.Text;
 
 namespace XSCTP
 {
+    internal
+#if !PROJECTN // readonly breaks codegen contract and asserts UTC
+    readonly
+#endif
+    ref struct ByReference<T>
+    {
+        // CS0169: The private field '{blah}' is never used
+#pragma warning disable 169
+        private readonly IntPtr _value;
+#pragma warning restore
+
+        [Intrinsic] //JitIntrinsic
+        public ByReference(ref T value)
+        {
+            // Implemented as a JIT intrinsic - This default implementation is for 
+            // completeness and to provide a concrete error if called via reflection
+            // or if intrinsic is missed.
+            throw new PlatformNotSupportedException();
+        }
+
+        public ref T Value
+        {
+            [Intrinsic]
+            get
+            {
+                // Implemented as a JIT intrinsic - This default implementation is for 
+                // completeness and to provide a concrete error if called via reflection
+                // or if the intrinsic is missed.
+                throw new PlatformNotSupportedException();
+            }
+        }
+    }
+
 
     //
     //readonly ref struct ReadOnlySpan<T>
 
-    ref struct BIO
+    unsafe ref  struct BIO
     {
-        public Span<byte> source;
+        public BIO(Span<byte> src)
+        {
+            _src = src;
+            CurPos = 0;
+            _psrc = (byte*)src.GetPinnableReference();
+        }
 
+        public Span<byte> _src;
+        private readonly ByReference<T> _psrc;
         private int CurPos;
 
-        public static (bool, int size) AvailableRead<T>(Span<byte> span, ref int curPos)
+        public static (bool, int size) ReadInc<T>(Span<byte> span, ref int curPos)
         {
             var size = Unsafe.SizeOf<T>();
             if (size > (span.Length - curPos))
             {
+                curPos = +size;
                 return (false, size);
                 //ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.length);
             }
@@ -30,13 +71,13 @@ namespace XSCTP
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ref T Read<T>() where T : unmanaged
         {
-            var (ok, size) = AvailableRead<T>(source, ref CurPos);
+            var (ok, size) = ReadInc<T>(_src, ref CurPos);
             if (ok)
             {
-                ref var v = ref Unsafe.As<byte, T>(ref source.GetPinnableReference());
+                ref var v = ref Unsafe.As<byte, T>(ref _src.Slice(CurPos, size).GetPinnableReference());
                 return ref v;
             }
-
+            return ref default(T);
         }
 
 
